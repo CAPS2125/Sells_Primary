3bca5a2691e6a9072210d4b91d89c9d38448252d
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-09-14 01:07:33 -0700

    Update 1_Dashboard.py
    Cambio en Importación de BD.

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from ..BD.tablas import SessionLocal, Ventas, DetalleVenta, Productos
+from BD.tablas import SessionLocal, Ventas, DetalleVenta, Productos
 from sqlalchemy import func
 import pandas as pd
 from datetime import date
@@ -65,3 +65,4 @@ if not df_mas_vendidos.empty:
     st.table(df_mas_vendidos)
 else:
     st.info("Aún no hay ventas registradas para hoy.")
+
