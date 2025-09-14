45fb12967cc8221df177630eab23c30c3687982a
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-09-14 00:50:27 -0700

    Update streamlit_app.py
    Sin Imagen.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,4 @@ st.set_page_config(
 
 st.title("Sistema de Gestión para la Tienda Escolar 🏫")
 st.markdown("Bienvenido al sistema de control de tu tienda de snacks saludables. Usa el menú de la izquierda para navegar por las diferentes secciones.")
-st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.es%2Ffotos-premium%2Fmesa-llena-alimentos-saludables-recreo-escolar-comida-equilibrada-ninos_37160352.htm&psig=AOvVaw3W-R9pTq1D7Vf2K3jP3KzT&ust=1726487140801000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDn8Mbyi4MDFQAAAAAdAAAAABAE")
-
 # La navegación se maneja automáticamente por Streamlit al colocar los archivos en la carpeta 'pages'.
