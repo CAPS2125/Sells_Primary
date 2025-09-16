321f09cc35be3c2f2d1a37d163eb5b70e7ab606c
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2025-09-16 15:31:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
 import streamlit as st
-from db import create_tables
-# Esta función se ejecutará solo una vez al iniciar la aplicación.
-# Streamlit maneja la ejecución de los scripts de las páginas de forma separada
-# pero el script principal solo se ejecuta una vez al iniciar.
-create_tables()
 
 st.set_page_config(
     page_title="Tienda Escolar",
